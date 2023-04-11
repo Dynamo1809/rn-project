fff4164dfccc3d2b6a4ffde7e426c5443f5746b0
DM <dynamo@DMs-MacBook-Air.local>
2023-04-11 06:06:19 +0300

    add background

--- App.js
+++ App.js
@@ -15,49 +15,12 @@ import {
   Keyboard,
   Alert,
   Button,
+  ImageBackground,
 } from 'react-native';
 
-// SplashScreen.preventAutoHideAsync();
-
 export default function App() {
-  // const [fontsLoaded] = useFonts({
-  //   'Roboto-Regular': require('./assets/fonts/Roboto/Roboto-Regular.ttf'),
-  //   'Roboto-Bold': require('./assets/fonts/Roboto/Roboto-Bold.ttf'),
-  // });
-  // const getFonts = async function () {
-  //   await Font.loadAsync({
-  //     'Roboto-Regular': require('./assets/fonts/Roboto/Roboto-Regular.ttf'),
-  //     'Roboto-Bold': require('./assets/fonts/Roboto/Roboto-Bold.ttf'),
-  //   });
-  // };
-
-  // const [isReady, setIsReady] = useState(false);
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
-
-  // useEffect(() => {
-  //   async function prepare() {
-  //     try {
-  //       await Font.loadAsync(Entypo.font);
-  //       await new Promise((resolve) => setTimeout(resolve, 2000));
-  //     } catch (e) {
-  //       console.warn(e);
-  //     } finally {
-  //       setIsReady(true);
-  //     }
-  //   }
-  //   prepare();
-  // }, []);
-
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded) {
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [fontsLoaded]);
-
-  // if (!fontsLoaded) {
-  //   return null;
-  // }
 
   const nameHandler = (text) => setName(text);
   const passwordHandler = (text) => setPassword(text);
@@ -65,25 +28,35 @@ export default function App() {
   const onLogin = () => {
     Alert.alert('Credentials:', `${name} + ${password}`);
   };
-// onLayout = { onLayoutRootView };
+  // onLayout = { onLayoutRootView };
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
-        <Text style={styles.title}>Welcome Roboto!</Text>
-        <Text style={{ fontSize: 20 }} >Welcome basic!</Text>
-        <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
-          <TextInput value={name} onChangeText={nameHandler} placeholder="Username" style={styles.input} />
-          <TextInput
-            value={password}
-            onChangeText={passwordHandler}
-            placeholder="Password"
-            secureTextEntry={true}
-            style={styles.input}
-          />
-          <Button title={'Login'} style={styles.input} onPress={onLogin} />
-        </KeyboardAvoidingView>
+        <ImageBackground style={styles.image} source={require('./images/beach.jpg')} resizeMode="cover">
+          <Text style={styles.title}>Welcome</Text>
+          <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
+            <View>
+              <TextInput
+                textAlign="center"
+                value={name}
+                onChangeText={nameHandler}
+                placeholder="Username"
+                style={styles.input}
+              />
+              <TextInput
+                textAlign="center"
+                value={password}
+                onChangeText={passwordHandler}
+                placeholder="Password"
+                secureTextEntry={true}
+                style={styles.input}
+              />
+              <Button title={'Login'} style={styles.input} onPress={onLogin} />
+            </View>
+          </KeyboardAvoidingView>
 
-        <StatusBar style="auto" />
+          <StatusBar style="auto" />
+        </ImageBackground>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -92,20 +65,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
-    fontFamily: 'Roboto-Bold',
     fontSize: 25,
+    marginBottom: 20,
   },
   input: {
+    color: '#fff',
+    fontSize: 20,
     width: 200,
     height: 44,
     padding: 10,
     borderWidth: 1,
     borderColor: 'black',
+    borderRadius: 10,
     marginBottom: 10,
+  },
+  image: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
